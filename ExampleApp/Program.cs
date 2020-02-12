bf850714aeeed5a90702a0caa292cbda7cb4b91c
Ettienne Scharneck <EttienneS@k2.com>
2020-02-12 11:37:44 +0200

    Cleanup example app

--- ExampleApp/Program.cs
+++ ExampleApp/Program.cs
@@ -7,20 +7,20 @@ namespace ExampleApp
     {
         private static void Main(string[] args)
         {
-            //try
-            //{
-            //    var menu = new Menu("\tExample :)", "Option 1", "Cheese", "That option with the long name");
-            //    var option = menu.GetOption();
+            try
+            {
+                var menu = new Menu("\tExample :)", "Option 1", "Cheese", "That option with the long name");
+                var option = menu.GetOption();
 
-            //    var name = ConsoleHelper.RequestInfo("Name", "Ettienne");
+                var name = ConsoleHelper.RequestInfo("Name", "Ettienne");
 
-            //    Console.WriteLine($"Selected: {option}");
-            //    Console.WriteLine($"Name: {name}");
-            //}
-            //catch (MenuCancelledException)
-            //{
-            //    // do something here
-            //}
+                Console.WriteLine($"Selected: {option}");
+                Console.WriteLine($"Name: {name}");
+            }
+            catch (MenuCancelledException)
+            {
+                // do something here
+            }
 
             var argsParser = new ArgsParser(
                     new StringArgument("FileName", "File", "F"),
